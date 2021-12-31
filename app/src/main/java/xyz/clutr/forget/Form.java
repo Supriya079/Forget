d84f7d3626275eb9dd24baa960fa2fc3bb9f309a
Supriya079 <supriyachaudhari140@gmail.com>
2021-12-31 09:47:05 +0530

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/xyz/clutr/forget/MainActivity.java
    #	app/src/main/res/layout/activity_form.xml

--- app/src/main/java/xyz/clutr/forget/Form.java
+++ app/src/main/java/xyz/clutr/forget/Form.java
@@ -2,16 +2,32 @@ package xyz.clutr.forget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Form extends AppCompatActivity {
+
+    View viewBackForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Objectsfragment()).commit();
+        //view binding with id's
+        viewBackForm = findViewById(R.id.viewFormBack);
+
+        viewBackForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //set fragment layout (ObjectsFragment) to frame layout of form.
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ObjectsFragment()).commit();
 
     }
 }
--- app/src/main/java/xyz/clutr/forget/ObjectsFragment.java
+++ app/src/main/java/xyz/clutr/forget/ObjectsFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Objectsfragment extends Fragment {
+public class ObjectsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
